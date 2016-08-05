c008e2770c53f9c7b393e11c89709a4aa48a936a
vinsonlin <vinson8733@sina.com>
2016-08-05 09:36:06 +0800

    Load flat-ui

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -8,8 +8,7 @@ class Home extends CI_Controller {
 	 */
 	public function index()
 	{
-		//$this->load->view('welcome_message');
-		echo  "hello world!";
+		$this->load->view('home/index.html');
 	}
 }
 ?>
